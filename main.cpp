0d0619279490e44649b33a9e087511e7117a51cb
ATadBitGhostly <reinisyo2@gmail.com>
2026-09-13 15:33:32 +0300

    First Commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+// Game Behaviour
+bool gameOver;
+
+// Window/Game size
+const int height = 20;
+const int width = 20;
+
+// Player
+int playerX, playerY;
+
+// Fruit
+int fruitX, fruitY;
+
+void Setup() {
+
+    gameOver = false;
+
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+int main() {
+
+}
