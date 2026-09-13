2dceea9229d038dc03e6cc46bf42bdd7e60f91fb
ATadBitGhostly <reinisyo2@gmail.com>
2026-09-13 15:57:44 +0300

    Map Boundaries Added

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ const int height = 20;
 const int width = 20;
 
 // Player
-int playerX, playerY;
+int playerX, playerY, score;
+enum eDIRECTION { UP, DOWN, LEFT, RIGHT, STOP = 0 };
+eDIRECTION dir;
 
 // Fruit
 int fruitX, fruitY;
@@ -17,10 +19,40 @@ int fruitX, fruitY;
 void Setup() {
 
     gameOver = false;
+    dir = STOP;
+    playerX = width / 2;
+    playerY = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
 
 }
 
 void Draw() {
+
+    system("cls"); // system clear in the console window
+    for (int i = 0; i < width; i++) {
+        std::cout << "#";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) {
+                std::cout << "#";
+            } else {
+                std::cout << " ";
+            }
+            if (j == width - 1) {
+                std::cout << "#";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < height; i++) {
+        std::cout << "#";
+    }
+    std::cout << std::endl;
 
 }
 
@@ -33,5 +65,9 @@ void Logic() {
 }
 
 int main() {
+
+    Draw();
+    Input();
+    Logic();
 
 }
